0f4ad6b94b86c96ffa84edc27050a111fea61482
Al DAmico <JavaWiz1@hotmail.com>
2025-01-13 23:32:49 -0500

    v0.2.2 - More re-packaging tweaks...

--- dt_tools/weather/weather.py
+++ dt_tools/weather/weather.py
@@ -20,10 +20,10 @@ import requests
 from loguru import logger as LOGGER
 
 import dt_tools.net.net_helper as nh
-from dt_tools.misc.census_geoloc import Census_GeoLocation
+from dt_tools.geoloc.census_geoloc import Census_GeoLocation
 from dt_tools.misc.api_helper import ApiTokenHelper
-from dt_tools.misc.sun import Sun
-from dt_tools.misc.weather.common import AQI_DESC, WeatherLocation, WeatherSymbols
+from dt_tools.geoloc.sun import Sun
+from dt_tools.weather.common import AQI_DESC, WeatherLocation, WeatherSymbols
 
 
 class CURRENT_WEATHER_SETTINGS:
